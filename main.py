cde622d534d14f0d12f6995444843629ec72eb6d
Yuki Ito <github@risaton.net>
2025-12-19 13:17:10 +0900

    Update info command embed in main.py for enhanced user guidance and presentation

--- main.py
+++ main.py
@@ -41,10 +41,21 @@ async def info(interaction: discord.Interaction):
     # embed.add_field(name="Version", value="2.0", inline=False)
     # embed.add_field(name="Creator", value="Yuki Ito", inline=False)
     # await interaction.response.send_message(embed=embed)
-    embed = discord.Embed(title='Created by Yuki.', description = 'バージョン: 0.1', color = 0x6fbfd3)
-    embed.set_author(name='OpenTTSBOTv2', url='https://risaton.net')
-    await interaction.response.send_message(embed=embed)
+    # embed = discord.Embed(title='Created by Yuki.', description = 'バージョン: 0.1', color = 0x6fbfd3)
+    # embed.set_author(name='OpenTTSBOTv2', url='https://risaton.net')
+    # await interaction.response.send_message(embed=embed)
+    embed = discord.Embed(title="OpenTTSBOTv2 v0.1",
+                          description="Created by Yuki.",
+                          colour=0x474fbd)
 
+    embed.add_field(name="コマンド",
+                    value="/join で読み上げを開始します。\n/leave で終了します。",
+                    inline=False)
+    embed.add_field(name="このボットについて",
+                    value="yuki1999によって作られた、オープンソースの読み上げBOTです！\ndiscordの仕様の移り変わりにて、以前のものは使えなくなってしまいましたが、1から作り直しました。",
+                    inline=False)
+
+    await interaction.response.send_message(embed=embed)
 
 
 bot.run(TOKEN)
